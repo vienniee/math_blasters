b86cba029dca5653bc982a2ae3f46744f111192e
junyan96 <71839331+junyan96@users.noreply.github.com>
2021-10-07 05:24:10 +0800

    amended

--- Game.py
+++ Game.py
@@ -21,7 +21,10 @@ font = pygame.font.SysFont('Times New Roman',26)
 #define colours
 red = (255,0,0)
 green = (0,255,0)
+blue = (0,0,255)
 black = (0,0,0)
+gray = (220,220,220)
+white = (0,0,0)
 
 #load images
 #background images
@@ -30,6 +33,8 @@ background_img = pygame.image.load('Image/castlebackground.png').convert_alpha()
 panel_img = pygame.image.load('Image/panel.png').convert_alpha()
 #button image
 button_img = pygame.image.load('Image/button.png').convert_alpha()
+#Question_panel
+questionpnl_img = pygame.image.load('Image/Qpanel.png').convert_alpha()
 
 #define gaming variables
 current_turn = 1
@@ -48,7 +53,7 @@ def draw_text(text,font,text_col,x,y):
 #questions with 4 options, last is answer
 questionlist = ["2",["1 + 1 - ?", "1", "2", "3", "4", "2"], ["2 + 2 = ?", "1", "2", "3", "4"],["3 + 3 = ?", "2", "4", "6", "8"]]
 question_list = questionlist[1:]
-print(randrange(len(question_list)))
+passingmark = int(questionlist[0])
 
 #function to randomise the question order
 def createRandomSortedList(num, start, end):
@@ -62,8 +67,11 @@ def createRandomSortedList(num, start, end):
 
 orderlist = createRandomSortedList(len(question_list),0,len(question_list)-1)
 
+reorderQlist = []
+for i in orderlist:
+    reorderQlist.append(question_list[i])
 
-
+print(reorderQlist)
 
 
 
@@ -80,6 +88,18 @@ def draw_pnl():
     #show stats
     draw_text(f'{player.name} HP:{player.hp}', font, red, 210, screen_height - bottom_panel + 5)
     draw_text(f'{enemies.name} HP:{enemies.hp}', font, red, 560, screen_height - bottom_panel + 5)
+
+class question_panel():
+    def __init__(self, x, y, image, wscale, hscale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width *wscale),int(height*hscale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        #draw panel on screen
+        screen.blit(self.image,(self.rect.x,self.rect.y))
 
 #define button
 #button class
@@ -119,6 +139,42 @@ option3_button = Button(20,530,button_img,0.55,0.175)
 option4_button = Button(440,530,button_img,0.55,0.175)
 abandon_button = Button(850,490,button_img,0.18,0.175)
 
+#create question panel
+quest_panel = question_panel(20,-130,questionpnl_img,1.7,1.3)
+
+#define curse
+class curse():
+    def __init__(self, x, y, scale):
+        self.animation_list = []
+        self.frame_index= 0
+        self.update_time = pygame.time.get_ticks()
+        #load ignite images
+        temp_list = []
+        for i in range(14):
+            img = pygame.image.load(f'Image/ignite/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width()*scale),int(img.get_height()*scale)))
+            self.animation_list.append(img)
+        self.image = self.animation_list[self.frame_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x,y)
+
+    def update(self):
+        animation_cooldown = 150
+        #update animation
+        self.image = self.animation_list[self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        if self.frame_index >= len(self.animation_list):
+            self.frame_index = len(self.animation_list) - 1
+
+
+    def draw(self):
+        screen.blit(self.image,self.rect)
+
+#create character instance
+playercurse = curse(280,290,2.5)
+enemiescurse = curse(650,290,2.5)
 
 #define player
 class character():
@@ -129,7 +185,6 @@ class character():
         self.alive = True
         self.animation_list = []
         self.frame_index= 0
-        #in progress pending for pixel arts
         self.action = 0#0:idle, 1:attack, 2:hurt, 3:death, if player answer question wrong, the enemies will attack
         self.update_time = pygame.time.get_ticks()
         #load idle images
@@ -232,6 +287,22 @@ class healthbar():
         pygame.draw.rect(screen,red,(self.x,self.y,150,20))
         pygame.draw.rect(screen, green, (self.x, self.y, 150*healthratio, 20))
 
+class progressbar():
+    def __init__(self,x,y,current,totalprogress):
+        self.x = x
+        self.y = y
+        self.current = current
+        self.totalprogress = totalprogress
+
+    def draw(self,current):
+        #update with new health
+        self.current = current
+        ratio = self.current / self.totalprogress
+        if ratio >= 1:
+            ratio = 1
+        pygame.draw.rect(screen,gray,(self.x, self.y,300,20))
+        pygame.draw.rect(screen, blue, (self.x, self.y, 300*ratio, 20))
+
 class DamageText(pygame.sprite.Sprite):
     def __init__(self, x, y, damage, colour):
         pygame.sprite.Sprite.__init__(self)
@@ -257,6 +328,8 @@ enemies = character(650,310,'enemies',len(questionlist)-1,2.5)
 player_hp = healthbar(210,screen_height-bottom_panel+40,player.hp,player.max_hp)
 enemies_hp = healthbar(560,screen_height-bottom_panel+40,enemies.hp,enemies.max_hp)
 
+progress_bar = progressbar(screen_width/3,40,enemies.max_hp-enemies.hp,passingmark)
+
 run = True
 while run:
     clock.tick(fps)
@@ -268,6 +341,10 @@ while run:
     player_hp.draw(player.hp)
     enemies_hp.draw(enemies.hp)
 
+    #draw progress bar
+    progress_bar.draw(enemies.max_hp-enemies.hp)
+
+
     #draw and update character animation
     player.update()
     player.draw()
@@ -277,6 +354,16 @@ while run:
     #damage text
     damage_text_group.update()
     damage_text_group.draw(screen)
+
+    quest_panel.draw()
+
+    if enemies.alive == False:
+        enemiescurse.update()
+        enemiescurse.draw()
+
+    if player.alive == False:
+        playercurse.update()
+        playercurse.draw()
 
     #draw button
     if option1_button.draw() == True:
