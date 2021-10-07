5a3056ccb7d48e0858d376fecd327fd67cf0075d
vienniee <vivienchew2000@gmail.com>
2021-10-08 00:27:44 +0800

    levelsel updated

--- levelselect.py
+++ levelselect.py
@@ -5,24 +5,30 @@ import random
 pygame.init()
 
 #create display window
-SCREEN_HEIGHT = 500
-SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600    
+SCREEN_WIDTH = 1000
 
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
 #load background image
+sky_surface = pygame.image.load('image\sky1.png').convert()
+ground_surface = pygame.image.load('image\ground1.png').convert()
 
 #load button images
 lvl1_img = pygame.image.load("image\Castle_level_1.png").convert_alpha()
 lvl2_img = pygame.image.load("image\Castle_level_2.png").convert_alpha()
 lvl3_img = pygame.image.load("image\Castle_level_3.png").convert_alpha()
 
+lvl1_img_rect = lvl1_img.get_rect()
+lvl2_img_rect = lvl1_img.get_rect()
+lvl3_img_rect = lvl1_img.get_rect()
+
 #load fonts
 #font style and size
 font = pygame.font.Font(None,18)
 level1text = font.render("LEVEL 1", True, [0,0,0])
 level2text = font.render("LEVEL 2", True,[0,0,0])
-level2text = font.render("LEVEL 3", True,[0,0,0])
+level3text = font.render("LEVEL 3", True,[0,0,0])
 
 
 #button class
@@ -55,18 +61,22 @@ class Button():
 
         return action
 
+print(lvl1_img_rect)
+
 #create button instances
 level1_button = Button(160,150,lvl1_img,2.5)
 level2_button = Button(310,150,lvl2_img,2.5)
 level3_button = Button(460,150,lvl3_img,2.5)
 
-
 #game loop
 run = True
 while run:
+    screen.fill((202, 228, 241))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
 
     #difficulty select
-    screen.fill((202, 228, 241))
+    
     if level1_button.draw() == True:
         print('level 1')
     if level2_button.draw() == True:
@@ -75,7 +85,11 @@ while run:
         print('level 3')
 
     screen.blit(level1text, (215,320))
-    #screen.blit(level2text,(320))
+    screen.blit(level2text, (415,320))
+    screen.blit(level3text, (615,320))
+
+    
+    
 
     #event handler
     for event in pygame.event.get():
