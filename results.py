0efdf850452393aec7b62f89cc13c9ab9def002c
ethel1211 <56384567+ethel1211@users.noreply.github.com>
2021-10-10 00:22:20 +0800

    finished results UI

--- results.py
+++ results.py
@@ -25,7 +25,7 @@ font = pygame.font.Font('freesansbold.ttf', 42)
 resultFont = pygame.font.Font('freesansbold.ttf', 58)
 scoreTextX = 390
 scoreTextY = 275
-resultTextX = 340
+resultTextX = 355
 resultTextY = 200
 
 
@@ -40,7 +40,42 @@ def showResults(x, y, passQuiz):
         screen.blit(resultText, (resultTextX-10, resultTextY))
 
 #TODO: continue button
+#try to take from levelselect later
 
+#button class
+class Button():
+    def __init__(self, x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width *scale),int(height*scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def draw(self):
+        action = False
+        #getting mouse position
+        pos = pygame.mouse.get_pos()
+
+        #check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                print('clicked')
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        #draw button on screen
+        screen.blit(self.image,(self.rect.x,self.rect.y))
+
+        return action
+
+cont = pygame.image.load('Image\cont.png').convert_alpha()
+
+#button instance
+contButton = Button(352,340,cont,0.25)
 
 
 
@@ -60,5 +95,8 @@ while running:
         showResults(scoreTextX,scoreTextY,True)
     else:
         showResults(scoreTextX,scoreTextY,False)
+
+    if contButton.draw() == True:
+        print('continue button selected')
     
     pygame.display.update()
