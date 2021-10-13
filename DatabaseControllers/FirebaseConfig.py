5153eeb9c2e5f427a19ef7df3caa2114ac958ee0
Melvin Chua <chua0946@e.ntu.edu.sg>
2021-10-13 23:17:32 +0800

    backend stuff

---
+++ DatabaseControllers/FirebaseConfig.py
@@ -0,0 +1,15 @@
+import pyrebase
+
+firebaseConfig = {
+  'apiKey': "AIzaSyBEOlShI29lUu4NhonKtqFH-NSt85ZvGVI",
+  'authDomain': "math-blasters.firebaseapp.com",
+  'projectId': "math-blasters",
+  'storageBucket': "math-blasters.appspot.com",
+  'messagingSenderId': "1022427765658",
+  'appId': "1:1022427765658:web:f5319e28731afca5783823",
+  'measurementId': "G-L2L2H64FDN",
+  "databaseURL": ""
+}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
