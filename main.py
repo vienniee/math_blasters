430407c2a3cf1da57677143719347d53cf1f5a38
Melvin Chua <chua0946@e.ntu.edu.sg>
2021-10-15 23:47:44 +0800

    change world selection to functional

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from pygame.locals import *
 import levelselect
 import Game
 import results
-
+import subject_chapter_selection
 #subject = worldselect()
 #topic =world.levelselect(subject)
 #level = levelselect.levelselect(topic)
 #score = minigame.score(topic, level)
 #results.storescore(subject, topic, level,score)
 
-
+subject_chapter_selection.subject_Chapter_selection()
 level = levelselect.levelselect('algebra')
 score = Game.Game('algebra',level)
 results.results('maths','algebra',level,score)
