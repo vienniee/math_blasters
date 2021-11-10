e8ceefd2843cd62a82c83bdb0185c704c0643f09
Melvin Chua <chua0946@e.ntu.edu.sg>
2021-11-11 00:38:23 +0800

    added quest house to chapter subject

--- WorldSelection/chapter_level.py
+++ WorldSelection/chapter_level.py
@@ -2,6 +2,7 @@ import pygame
 from Castle import Castle
 from Portal import Portal
 from CastleText import CastleText
+from Quest_House import Quest_House
 
 
 def load_assets(chap_1, chap_1_level, chap_2, chap_2_level):
@@ -11,13 +12,17 @@ def load_assets(chap_1, chap_1_level, chap_2, chap_2_level):
     castles.add(Castle(800, chap_2_level, chap_2))
 
     castlesNames = pygame.sprite.Group()
-    castlesNames.add(CastleText(200, chap_1))
+    castlesNames.add(CastleText(400, chap_1))
     castlesNames.add(CastleText(800, chap_2))
 
     backPortal = pygame.sprite.GroupSingle()
     backPortal.add(Portal(100,1))
 
-    return castles,castlesNames,backPortal
+    castlesNames.add(CastleText(1200, "Quest"))
+    questHouse = pygame.sprite.GroupSingle()
+    questHouse.add(Quest_House(1200))
+
+    return castles,castlesNames,backPortal,questHouse
 
 # def load_backPortal():
 #     backPortal = pygame.sprite.GroupSingle()
@@ -27,10 +32,11 @@ def load_assets(chap_1, chap_1_level, chap_2, chap_2_level):
 #     return backPortal
 
 
-def chapter_selection(player,screen, castles, castleName, backPortal):
+def chapter_selection(player,screen, castles, castleName, backPortal, questHouse):
     castles.draw(screen)
     castleName.draw(screen)
     backPortal.draw(screen)
+    questHouse.draw(screen)
     player.draw(screen)
 
     player.update()
@@ -46,6 +52,12 @@ def check_castles(keys, castles, teleportCooldownstate, player):
     if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and collision_sprite(player, castles) and not teleportCooldownstate:
         print("teleport to chapter")
         return True
+
+def check_quest_house(keys, questHouse, teleportCooldownstate, player):
+    if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and collision_sprite(player, questHouse) and not teleportCooldownstate:
+        print("go to quest select")
+        return True
+
 
 
 
